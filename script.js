ec86ef8d7187ceb39c7c03da0005ad0bc37a63f6
waliulu14 <infowebprogram@gmail.com>
2023-07-20 09:36:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+// buton
+
+$( function() {
+    $( ".dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( ".dialog" ).dialog( "open" );
+    });
+  } );
+
